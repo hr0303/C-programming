ad54f52ca1f5d055fe69aa593aadaaddc827fe8b
hr0303 <111736573+hr0303@users.noreply.github.com>
2022-09-25 16:31:40 +0530

    Create Area_of_rectangle.c

---
+++ Area_of_rectangle.c
@@ -0,0 +1,17 @@
+//Area of rectangle 
+
+#include <stdio.h>
+
+void main()
+{
+    float l,b,area;
+    /* l= lenght of the rectangle
+       b= width of the recatnagle */
+    printf("Enter the length:  ");
+    scanf("%f",&l);
+    printf("Enter the value of width: ");
+    scanf("%f",&b);
+    
+    area=l*b;
+    printf("Area of the recatngle is :%f ",area);
+}
