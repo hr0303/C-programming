ba82aed4729ff39a74ede764560b8f4766ced972
hr0303 <111736573+hr0303@users.noreply.github.com>
2022-09-25 11:07:23 +0530

    Update printing_alphabets_AtoZ.c

--- printing_alphabets_AtoZ.c
+++ printing_alphabets_AtoZ.c
@@ -10,3 +10,16 @@ int main()
        
     }
 }
+
+
+
+// For printing alphabets from A to desired aplhabet
+
+#include <stdio.h>
+int main()
+{
+    char c,n;
+    scanf("%c",&n);
+    for(c='A';c<=n;c++)
+    printf("%c ",c);
+}
