d71f38ed55f45364e3a97b8f90d0ec173b912d21
hr0303 <111736573+hr0303@users.noreply.github.com>
2022-09-25 12:45:24 +0530

    Update pattern_2.c

--- pattern_2.c
+++ pattern_2.c
@@ -1,5 +1,5 @@
 /*
-I/P = 4
+I/P = 4 (no. of rows)
 O/P = A 
       B C 
       D E F 
@@ -23,4 +23,34 @@ int main() {
         }
         printf("\n");
     }
+}
+
+
+
+// If input is taken for LAST CHARACTER TO BE PRINTED
+// Input is case sensitive (O/P will be different for j & J); it will follow ascai order.
+
+#include <stdio.h>
+
+int main() {
+    int i,j,m;
+    char c,n;
+    c='A';
+    printf("ENTER THE LAST CHARACTER TO BE PRINTED ");
+    scanf("%c",&n);
+    
+    for(i=1;;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            if(c<=n){
+                printf("%c ",c);
+                c++;
+            }
+            else
+                break;
+        }
+        printf("\n");
+    }
+    
 }
