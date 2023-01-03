399ab6228aac9358bf65d489d68a00a2521de853
hr0303 <111736573+hr0303@users.noreply.github.com>
2023-01-03 22:45:21 +0530

    Create array qu1.c

---
+++ array qu1.c
@@ -0,0 +1,29 @@
+//FOR AN ARRAY OF SIZE n find total no of odd and even integers
+/*eg: I/P: n=8,arr = [3,3,4,3,1,5,7,8]
+      O/P: odd=6, even=2   */
+
+
+#include <stdio.h>
+
+int main() {
+    int n,a[100],i,even=0,odd=0;
+    
+    printf("Enter array size: ");
+    scanf("%d",&n);
+    
+     printf("Enter array elements: ");
+     for(i=1;i<=n;i++)
+     scanf("%d",&a[i]);
+     
+     for(i=1;i<=n;i++)
+     {
+         if(a[i]%2==0)
+         even=even+1;
+         else
+         odd=odd+1;
+     }
+     
+     printf("No of even elements in the array= %d",even);
+     printf("\nNo of odd elements in the array= %d",odd);
+    return 0;
+}
